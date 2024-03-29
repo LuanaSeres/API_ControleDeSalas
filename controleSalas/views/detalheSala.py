e14dc5d740652a604050cebff29b786baaa72d38
Talia-goncalves <gtalia246@gmail.com>
2024-03-28 21:43:11 -0300

    feat(controleSalas): criando a função de atualizar sala

--- controleSalas/views/detalheSala.py
+++ controleSalas/views/detalheSala.py
@@ -1,7 +1,9 @@
 from django.views import View
 from django.shortcuts import render
+from django.http import JsonResponse
 from repository import RepositorioSalas
 from ..serializers.SalasSerializers import SalasSerializers
+import json
 
 class DetalhesSalaView(View):
     def get(self, request, id):
@@ -9,6 +11,18 @@ class DetalhesSalaView(View):
         try:
             sala = repository.get_sala_by_id(id)
             serializer = SalasSerializers(sala)
-            return render(request, "detalhesSala.html", {"sala": serializer.data})
+            return JsonResponse(serializer.data)
         except RepositorioSalas.SalaNaoEncontradaException:
-            return render(request, "salaNaoEncontrada.html")
+            return JsonResponse({'error': 'Sala de aula não encontrada'}, status=404)
+
+    def put(self, request, id):
+        repository = RepositorioSalas()
+        try:
+            sala = repository.get_sala_by_id(id)
+            serializer = SalasSerializers(sala, data=json.loads(request.body))
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=400)
+        except RepositorioSalas.SalaNaoEncontradaException:
+            return JsonResponse({'error': 'Sala de aula não encontrada'}, status=404)
